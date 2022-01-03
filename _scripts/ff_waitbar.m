487fbb7c479852b219283cffa4cecc2e946f847e
gdsmith5 <92103528+gdsmith5@users.noreply.github.com>
2022-01-03 17:15:52 -0500

    update

--- _scripts/ff_waitbar.m
+++ _scripts/ff_waitbar.m
@@ -5,14 +5,11 @@ lim = 5000;   % # of terms in expansion
 x = 0.2;   % value of exponent
 taylor = 0;
 
-tic
 f = waitbar(0,'Progress');   % establish progress bar
 for i = 0:lim
    waitbar(i/lim,f,'Progress');    % update progress bar every iteration
    taylor = taylor + x^i/factorial(i);   % add term to Taylor expansion
 end
-toc
 
-% waitbar(0.67,f,'Progress')
 close(f)   % close progress bar
 taylor - exp(x)
--- index_GDS.markdown
+++
@@ -1,8 +0,0 @@
----
-layout: home
-title: mmm
-subtitle: Musings on MATLAB and Machine Learning
----
-
-{: .box-note}
-I teach a graduate course on machine learning in Chemistry using MATLAB. This is my blog of [**MATLAB**](/tags#MATLAB) tips & tricks and [**Machine Learning (ML)**](/tags#ML) explanations that students have found helpful over the years.
